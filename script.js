0ccdf80eab4b105ea636427880391246b48a8d60
Junior Baez <junior_baez@ucne.edu.do>
2022-06-05 21:58:23 -0400

    Creating the model that the will be used

--- script.js
+++ script.js
@@ -37,3 +37,20 @@
   }
   
   document.addEventListener('DOMContentLoaded', run);
+
+  function createModel() {
+    // Create a sequential model
+    const model = tf.sequential();
+  
+    // Add a single input layer
+    model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+  
+    // Add an output layer
+    model.add(tf.layers.dense({units: 1, useBias: true}));
+  
+    return model;
+  }
+
+  // Create the model
+const model = createModel();
+tfvis.show.modelSummary({name: 'Model Summary'}, model);
